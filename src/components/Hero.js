4eeb1f929a0fcdc111ca2ecf9d29701e71658539
suryanshvns <suryanshvns2603@gmail.com>
2024-06-24 00:57:59 +0530

    issue fixed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,9 @@
 const Hero = () => {
   return (
-    <section id="hero" className="bg-slate-800	 text-white h-screen flex items-center justify-center">
+    <section id="hero" className="bg-slate-800 text-white h-screen flex items-center justify-center">
       <div className="text-center">
-        <h2 className="text-4xl md:text-6xl font-bold mb-4">Hello, I'm Suryansh Raghuvanshi</h2>
-        <p className="text-xl md:text-2xl text-black	">I'm a Full-Stack Web and mobile Developer</p>
+        <h2 className="text-4xl md:text-6xl font-bold mb-4">Hello, I&apos;m Suryansh Raghuvanshi</h2>
+        <p className="text-xl md:text-2xl text-black">I&apos;m a Full-Stack Web and Mobile Developer</p>
       </div>
     </section>
   );
